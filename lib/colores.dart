cfcdcd1296853414c877e47803cfc2928557c635
Alejandro Ordaz <oordaz@nrtec.com.mx>
2023-11-16 07:34:32 -0600

    Marcas personlisadas

--- lib/colores.dart
+++ lib/colores.dart
@@ -1,5 +1,6 @@
 import 'package:apis/getApi.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Vista2 extends StatefulWidget {
   const Vista2({super.key});
@@ -18,28 +19,178 @@ class _Vista2State extends State<Vista2> {
         children: [
           Column(
             children: [
-              val.isNotEmpty ? Row(
-                children: [
-                  //CONTENEDORES
-                ],
-              ) : Container()
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 15, bottom: 15),
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: FloatingActionButton.large(
-                onPressed: () {
-                  setState(() {
-                    getJsonData();
-                  });
-                },
-                backgroundColor: Colors.blueAccent,
-                child: const Text(
-                  'Solicitar los colores', textAlign: TextAlign.center,
+              Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                margin: EdgeInsets.all(15),
+                elevation: 10,
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
+                      title: Text('Nombre apellidoP apellidoM'),
+                      subtitle: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Correo electronico'),
+                              Text('telefono')
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Calificacion '),
+                              Text('Edad '),
+                              Text('Pais')
+                            ],
+                          ),
+                        ],
+                      ),
+
+                      leading: Icon(Icons.person),
+                    ),
+
+                    // Usamos una fila para ordenar los botones del card
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        IconButton(onPressed: (){}, icon: Icon(Icons.update, color: Colors.blue,)),
+                        IconButton(onPressed: (){}, icon: Icon(Icons.delete, color: Colors.red,)),
+                      ],
+                    )
+                  ],
                 ),
               ),
+              Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                margin: EdgeInsets.all(15),
+                color: Color(0xA900FFF5),
+                elevation: 10,
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
+                      title: Text('Nombre apellidoP apellidoM'),
+                      subtitle: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('a@a.com'),
+                              Text('444 123 4567')
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('100', style: TextStyle(color: Colors.green, fontSize: 20),),
+                              Text('Edad '),
+                              Text('Mexico')
+                            ],
+                          ),
+                        ],
+                      ),
+
+                      leading: Icon(Icons.person),
+                    ),
+
+                    // Usamos una fila para ordenar los botones del card
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        IconButton(onPressed: (){}, icon: Icon(Icons.update, color: Colors.blue,)),
+                        IconButton(onPressed: (){}, icon: Icon(Icons.delete, color: Colors.red,)),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                margin: EdgeInsets.all(15),
+                color: Colors.black,
+                elevation: 10,
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(15, 10, 25, 0),
+                      title: Text('Nombre apellidoP apellidoM', style: TextStyle(color: Colors.white),),
+                      subtitle: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Correo electronico', style: TextStyle(color: Colors.white)),
+                              Text('telefono', style: TextStyle(color: Colors.white))
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('65 ', style: TextStyle(color: Colors.orange)),
+                              Text('Edad ', style: TextStyle(color: Colors.white)),
+                              Text('Alemania', style: TextStyle(color: Colors.white),)
+                            ],
+                          ),
+                        ],
+                      ),
+
+                      leading: Icon(Icons.person),
+                    ),
+
+                    // Usamos una fila para ordenar los botones del card
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        IconButton(onPressed: (){}, icon: Icon(Icons.update, color: Colors.blue,)),
+                        IconButton(onPressed: (){}, icon: Icon(Icons.delete, color: Colors.red,)),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          IconButton(
+              onPressed: (){
+                setState(() {
+                  _textMe();
+                });
+              },
+              icon: Icon(Icons.sms),
+          ),
+          IconButton(
+            onPressed: (){
+              setState(() {
+                _callMe();
+              });
+            },
+            icon: Icon(Icons.call),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, top: 30, right: 30, bottom: 15),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red, // background
+                  onPrimary: Colors.yellow, // foreground
+                ),
+                onPressed: () {
+                  setState(() {
+                    _textMe();
+                    _callMe();
+                  });
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(Icons.refresh),
+                    Text('Solicitar nuevos usuarios')
+                  ],
+                ),
+              )
             ),
           )
         ],
@@ -47,11 +198,37 @@ class _Vista2State extends State<Vista2> {
     );
   }
 
+  /*void _sendSMS(String message, List<String> recipents) async {
+    String _result = await sendSMS(message: message, recipients: recipents, sendDirect: true)
+        .catchError((onError) {
+      print(onError);
+    });
+    print(_result);
+  }*/
+
+  _callMe() async {
+    launch('tel: 4443218456');
+  }
+  _textMe() async {
+    // Android
+    // %20 = el espacio
+    const uri = 'sms:+4443218456?body=hello%20there%20all';
+    if (await canLaunch(uri)) {
+      await launch(uri);
+    } else {
+      // iOS
+      const uri = 'sms:0039-222-060-888?body=hello%20there';
+      if (await canLaunch(uri)) {
+        await launch(uri);
+      } else {
+        throw 'Could not launch $uri';
+      }
+    }
+  }
+
   Future<void> getJsonData() async {
     //Llamado a la clase
-    Colores colores = Colores(
-      nombre: 'Alejandro Ordaz',
-    );
+    Colores colores = Colores( nombre: 'Alejandro Ordaz', );
 
     try {
       var data;
@@ -91,3 +268,7 @@ class _Vista2State extends State<Vista2> {
     return num;
   }
 }
+
+//marker dynamic
+//webview lat, lng atravez de una dirección
+
--- lib/mapa.dart
+++ lib/mapa.dart
@@ -2,6 +2,7 @@ import 'package:apis/colores.dart';
 import 'package:apis/getApi.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
 
 class Mapa extends StatefulWidget {
   const Mapa({super.key});
@@ -13,16 +14,22 @@ class Mapa extends StatefulWidget {
 class _MapaState extends State<Mapa> {
   late GoogleMapController mapController;
 
-  late LatLng _center = const LatLng(45.521563, -122.677433);
+  late LatLng _center = const LatLng(22.144596, -101.009064);
 
   //Guardar los puntos con las coordenadas (lat, lng)
   final List<LatLng> polyPoints = [];
   //Guardar las lineas sobre el mapa
   final Set<Polyline> polyLines = {};
 
+  final List<Marker> _markers = <Marker>[];
+
+  static const LatLng sourceLocation = LatLng(22.144596, -101.009064);
+  static const LatLng destination = LatLng(22.14973, -100.992221);
+
   @override
   void initState() {
     getJsonData(); // Función que realiza el llamado a la api
+    setCustomMarkerIcon(); // Se encarga de crear nuestras marcas personalizadas
     super.initState();
   }
 
@@ -80,6 +87,46 @@ class _MapaState extends State<Mapa> {
     });
   }
 
+  createMarker() async {
+    String iconurl ="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Catedral_SLP_cielo.jpg/1200px-Catedral_SLP_cielo.jpg";
+    var dataBytes;
+    var request = await http.get(Uri.parse(iconurl));
+    var bytes = await request.bodyBytes;
+
+    setState(() {
+      dataBytes = bytes;
+    });
+
+    LatLng _lastMapPositionPoints = LatLng(
+        double.parse("22.142435"),
+        double.parse("-101.009346"));
+
+    _markers.add(Marker(
+      icon: BitmapDescriptor.fromBytes(dataBytes.buffer.asUint8List()),
+      markerId: MarkerId(_lastMapPositionPoints.toString()),
+      position: _lastMapPositionPoints,
+      infoWindow: const InfoWindow(
+        title: "Delivery Point",
+        snippet:
+        "My Position",
+      ),
+    ));
+  }
+
+
+
+
+
+  //Imagen -> Marca para visualizar en el mapa
+  BitmapDescriptor sourceIcon = BitmapDescriptor.defaultMarker;
+  void setCustomMarkerIcon() {
+    BitmapDescriptor.fromAssetImage(
+        ImageConfiguration.empty, "img/icon.png").then((icon) {
+        sourceIcon = icon;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,6 +135,24 @@ class _MapaState extends State<Mapa> {
           GoogleMap(
             onMapCreated: _onMapCreated,
             polylines: polyLines,
+            markers: {
+              const Marker(
+                markerId: MarkerId("source"),
+                position: sourceLocation,
+                infoWindow: InfoWindow(title: "Información inicio"),
+              ),
+              Marker(
+                markerId: MarkerId("destination"),
+                position: destination,
+                icon: sourceIcon,
+                infoWindow: const InfoWindow(title: "Información destino"),
+                onTap: () {
+                  setState(() {
+                    showAlertDialog(context);
+                  });
+                }
+              ),
+            },
             initialCameraPosition: CameraPosition(
               target: _center,
               zoom: 11.0,
@@ -112,6 +177,37 @@ class _MapaState extends State<Mapa> {
           )
         ],
       ),
+    );
+  }
+
+  showAlertDialog(BuildContext context) {
+
+    // set up the buttons
+    Widget cancelButton = TextButton(
+      child: Text("Cancel"),
+      onPressed:  () {},
+    );
+    Widget continueButton = TextButton(
+      child: Text("Continue"),
+      onPressed:  () {},
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("AlertDialog"),
+      content: Text("Would you like to continue learning how to use Flutter alerts?"),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 }
